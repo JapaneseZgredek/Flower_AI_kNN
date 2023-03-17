67ce02032cd559dc59caaaeb3cbddc3fa46ea6f9
Fabian Zbrański <fabian.zbranski@gmail.com>
2023-03-17 19:34:06 +0100

    Pass list in Flower.py __init__ instead of creating one, read from file to list of Flower.py

--- mpp1/main.py
+++ mpp1/main.py
@@ -1,23 +1,37 @@
+from Flower import Flower
 def read_from_file() -> list:
     chosen_file = int(input("Which file are you going to choose: \n "
                             "1.iris_test.txt \n 2.iris_training.txt \n"))
     path = ""
     list_of_flowers_to_return = []
+
     if chosen_file == 1:
         path = r"data/iris_test.txt"
     elif chosen_file == 2:
         path = r"data/iris_training.txt"
     else:
         print("Wrong input, are you really such a dummy???")
+
     with open(path, "r") as file:
         read_content = file.read()
-        data_in_list = read_content.split("\t")
-        
-    return []
+        data_in_lines = read_content.split("\n")
+        for line in data_in_lines:
+            line_as_list = line.split("\t")
+            if len(line_as_list) == 1:
+                continue
+            for i in range(len(line_as_list)):
+                line_as_list[i] = line_as_list[i].strip()
+            print(line_as_list)
+            list_of_flowers_to_return.append(Flower(line_as_list[:len(line_as_list)-1], line_as_list[len(line_as_list)-1]))
+
+    return list_of_flowers_to_return
 
 
 def main():
     data = read_from_file()
+    for flower in data:
+        print(flower.attributes, end=" ")
+        print(flower.flower_type)
 
 
 if __name__ == '__main__':
