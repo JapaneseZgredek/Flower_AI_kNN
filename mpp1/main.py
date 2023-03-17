5aa30a87b6c4d13c8f02eab57bb18634c8e42b0b
Fabian Zbrański <fabian.zbranski@gmail.com>
2023-03-17 19:59:01 +0100

    refactor code, because we have to read both files everytime

--- mpp1/main.py
+++ mpp1/main.py
@@ -1,16 +1,12 @@
 from Flower import Flower
-def read_from_file() -> list:
-    chosen_file = int(input("Which file are you going to choose: \n "
-                            "1.iris_test.txt \n 2.iris_training.txt \n"))
-    path = ""
-    list_of_flowers_to_return = []
-
+def which_path(chosen_file):
     if chosen_file == 1:
-        path = r"data/iris_test.txt"
+        return r"data/iris_test.txt"
     elif chosen_file == 2:
-        path = r"data/iris_training.txt"
-    else:
-        print("Wrong input, are you really such a dummy???")
+        return r"data/iris_training.txt"
+def read_from_file(chosen_file) -> list:
+    list_of_flowers_to_return = []
+    path = which_path(chosen_file)
 
     with open(path, "r") as file:
         read_content = file.read()
@@ -21,15 +17,19 @@ def read_from_file() -> list:
                 continue
             for i in range(len(line_as_list)):
                 line_as_list[i] = line_as_list[i].strip()
-            print(line_as_list)
             list_of_flowers_to_return.append(Flower(line_as_list[:len(line_as_list)-1], line_as_list[len(line_as_list)-1]))
 
     return list_of_flowers_to_return
 
 
 def main():
-    data = read_from_file()
-    for flower in data:
+    training_data = read_from_file(2)
+    test_data = read_from_file(1)
+    for flower in training_data:
+        print(flower.attributes, end=" ")
+        print(flower.flower_type)
+    print("---------------------")
+    for flower in test_data:
         print(flower.attributes, end=" ")
         print(flower.flower_type)
 
