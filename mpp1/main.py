f87c388fa6d0e5477809757be282846eb3abbff5
Fabian Zbrański <fabian.zbranski@gmail.com>
2023-03-18 16:06:11 +0100

    Check what type is flower under investigation, start creating user input possibility

--- mpp1/main.py
+++ mpp1/main.py
@@ -6,15 +6,20 @@ class Counter:
         self.iris_virginica = 0
 
     def count(self, flower_type: str):
-        if flower_type is "Iris-setosa":
+        if flower_type == "Iris-setosa":
             self.iris_setosa = self.iris_setosa + 1
-        elif flower_type is "Iris-versicolor":
+        elif flower_type == "Iris-versicolor":
             self.iris_versicolor = self.iris_versicolor + 1
-        elif flower_type is "Iris-virginica":
+        elif flower_type == "Iris-virginica":
             self.iris_virginica = self.iris_virginica + 1
 
-    def which_one(self) -> int:
-        return max(self.iris_setosa, self.iris_versicolor, self.iris_virginica)
+    def which_one(self) -> str:
+        if self.iris_setosa >= self.iris_virginica and self.iris_setosa >= self.iris_versicolor:
+            return "Iris-setosa"
+        elif self.iris_virginica >= self.iris_setosa and self.iris_virginica >= self.iris_versicolor:
+            return "Iris-virginica"
+        elif self.iris_versicolor >= self.iris_setosa and self.iris_versicolor >= self.iris_virginica:
+            return "Iris-versicolor"
 def which_path(chosen_file):
     if chosen_file == 1:
         return r"data/iris_test.txt"
@@ -46,14 +51,29 @@ def main():
     test_data = read_from_file(1)
     reformat_data_commas_to_dots(training_data)
     reformat_data_commas_to_dots(test_data)
-    # how_many_neighbours = int(input('What will be the k value, in the kNN algorithm: '))
+    how_many_neighbours = int(input('What will be the k value, in the kNN algorithm: '))
+    correct_guesses = 0
     for current_flower_test in test_data:
+        counter = Counter()
         distances = []
+
         for current_flower_training in training_data:
             distances.append((current_flower_test.calculate_distance(current_flower_training), current_flower_training.flower_type))
         distances.sort(key=lambda a: a[0])
 
-        print(distances)
+        for neighbour in range(how_many_neighbours):
+            counter.count(distances[neighbour][1])
+        if counter.which_one() == current_flower_test.flower_type:
+            correct_guesses = correct_guesses + 1
+
+    print(str(correct_guesses) + " " + str(round((correct_guesses * 100) / len(test_data), 2)) + "%")
+
+    does_user_want_to_input = int(input("0 - I want to input my own attributes \n1 - End the program\n")) == 0
+    while does_user_want_to_input:
+        user_input = input("Give me values separated by spaces, and with dots as number pointer,"
+                           " NOT COMMAS LIKE IN DATA GIVEN BY FABULOUS UNIVERSITY, in this program version required number of attributes is "
+                           + str(len(test_data[0].attributes)) + ":").split(" ")
+
 
 
 if __name__ == '__main__':
